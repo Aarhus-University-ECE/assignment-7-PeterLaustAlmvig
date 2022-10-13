4ee359813191c3c1fe3fc845c87e720701d38340
Peter Laust Almvig <petealmvig@gmail.com>
2022-10-13 14:37:00 +0200

    Added stack functions xd

--- src/stack.c
+++ src/stack.c
@@ -1,25 +1,39 @@
 #include "stack.h"
+#include <assert.h>
 
 void initialize(stack* s){
   //implement initialize here
+  s->head = NULL;
+
 }
 
 void push(int x, stack* s){
     //implement push here
+    node *element = (node* ) malloc(sizeof(node));
+    element->next = s->head;
+    element->data = x;
+    s->head = element;
 }
 
 int pop(stack* s){
     // implement pop here
-  return -1;
+    assert(s->head != NULL);
+    int popped_element = s->head->data;
+    s->head = s->head->next;
+  return popped_element;
 }
 
 bool empty(stack* s)
 {
   //implement empty here
+  if(s->head == NULL){
+    return true;
+  }
   return false;
 }
 
 bool full(stack* s) {
     //implement full here
+    //Doesn't make sense to check if the stack is full.
   return false;
 }
