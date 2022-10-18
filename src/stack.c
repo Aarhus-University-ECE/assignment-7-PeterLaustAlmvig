21631f214c9e298b91d0b747144845341ac9ec6d
Peter Laust Almvig <petealmvig@gmail.com>
2022-10-18 14:17:52 +0200

    Added comments to stack.c

--- src/stack.c
+++ src/stack.c
@@ -3,12 +3,16 @@
 
 void initialize(stack* s){
   //implement initialize here
+  //Once the stack is initialised the head must point to NULL since there's no elements.
   s->head = NULL;
 
 }
 
 void push(int x, stack* s){
     //implement push here
+    /*Create a new element then make that element point to the previous top of the stack (s.head)
+    * Update s.head to point to the newest top element and add the number to elements datafield.
+    */
     node *element = (node* ) malloc(sizeof(node));
     element->next = s->head;
     element->data = x;
@@ -17,6 +21,9 @@ void push(int x, stack* s){
 
 int pop(stack* s){
     // implement pop here
+    /*Only pop an element if the stack is not empty.
+    * make the head point to the second element from the top and return the old s.head data value.
+    */
     assert(s->head != NULL);
     int popped_element = s->head->data;
     s->head = s->head->next;
@@ -26,6 +33,7 @@ int pop(stack* s){
 bool empty(stack* s)
 {
   //implement empty here
+  //The stack is only empty if s.head points to NULL.
   if(s->head == NULL){
     return true;
   }
@@ -34,6 +42,8 @@ bool empty(stack* s)
 
 bool full(stack* s) {
     //implement full here
-    //Doesn't make sense to check if the stack is full.
+    /*Doesn't make sense to check if the stack is full since we haven't declared a stack size
+    * The stack will be full once we run out of memeory.
+    */
   return false;
 }
